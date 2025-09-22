7758ac92012847f1c1ec109e19faf0facad7f8e9
dulumary <lecture@hagulu.com>
2025-09-22 21:15:50 +0900

    [김인규]  - Marondalgram 프로젝트 생성  - 기본 환경 설정

---
+++ src/main/java/com/marondal/marondalgram/hello/HelloController.java
@@ -0,0 +1,20 @@
+package com.marondal.marondalgram.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!!";
+    }
+
+    @GetMapping("/hello/html")
+    public String helloHtml() {
+        return "hello/hello";
+    }
+}
